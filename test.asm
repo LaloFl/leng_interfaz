52944be426ca3401920452b67cf86e8d65655306
diego <defsdiego@gmail.com>
2022-09-18 03:56:41 -0700

    examen opc1 terminada

--- test.asm
+++ test.asm
@@ -2,14 +2,25 @@
 .stack 50
 
 .data
-str db 'hola $'
+str_ db 'hola $'
 
 .code
 mov ax, @data
-mov ds, ax ; LECTURA DE .data
+mov ds, ax
 
-mov ah, 09
-lea dx, str
+mov ah, 0
+mov al, 3
+int 10h ; LIMPIAR
+
+
+mov dl, ncol
+mov dh, nrow
+mov  bh, 0
+mov  ah, 02h
+int 10h
+
+mov ah, 9
+lea dx, str_
 int 21h
 
 .exit
--- u1_exa.asm
+++ u1_exa.asm
@@ -54,6 +54,8 @@ var_asist db 0
 var_mate_p db 0
 var_hist_p db 0
 var_quim_p db 0
+var_prom db 0
+var_porc_asist db 0
 
 ; PREGUNTAS
 str_1_title db '------CAPTURA E IMPRESION DE BOLETA------ $'
@@ -64,7 +66,6 @@ str_1_nomesc db 'Ingresa el nombre de tu escuela: $'
 str_1_grupo db 'Ingresa tu grupo: $'
 str_1_turno db 'Ingresa tu turno: $'
 str_1_nctrl db 'Ingresa tu no. de control: $'
-
 str_1_mate1 db '(MATE, parcial 1) Ingresa tu calif.: $'
 str_1_mate2 db '(MATE, parcial 2) Ingresa tu calif.: $'
 str_1_mate3 db '(MATE, parcial 3) Ingresa tu calif.: $'
@@ -74,7 +75,6 @@ str_1_quim3 db '(QUIM, parcial 3) Ingresa tu calif.: $'
 str_1_hist1 db '(HIST, parcial 1) Ingresa tu calif.: $'
 str_1_hist2 db '(HIST, parcial 2) Ingresa tu calif.: $'
 str_1_hist3 db '(HIST, parcial 3) Ingresa tu calif.: $'
-
 str_1_asist db 'Ingresa tu numero de asistencias (9 total): $'
 
 ; FORMATO
@@ -84,16 +84,16 @@ str_1_row_ex db '     ==================        ==================         =====
 str_1_row_04 db '      APELLIDO PATERNO          APELLIDO MATERNO              NOMBRE(S)         $'
 str_1_row_05 db 'DATOS DE LA ESCUELA                                                             $'
 str_1_row_06 db '                                                                                $'
-str_1_row_07 db '    ========================     =======         ============          =====    $'
-str_1_row_08 db '      NOMBRE DE LA ESCUELA        GRUPO             TURNO               CCT     $'
+str_1_row_07 db '    ========================     =======         ============      ==========   $'
+str_1_row_08 db '      NOMBRE DE LA ESCUELA        GRUPO             TURNO           #CONTROL    $'
 str_1_row_09 db '|--------------------------------------------------| |-----------------------|  $'
 str_1_row_10 db '|                 |PERIODOS DE EVALUACION |        | |       ASISTENCIA      |  $'
 str_1_row_11 db '|ASIGNATURAS/AREAS|-----------------------|PROMEDIO| |-----------------------|  $'
-str_1_row_12 db '|                 |  1RO  |  2DO  |  3RO  |        | |CALENDARIO ESCOLAR| 9  |  $'
+str_1_row_12 db '|                 |  1RO  |  2DO  |  3RO  |        | |CALENDARIO ESCOLAR| 10 |  $'
 str_1_row_13 db '|-----------------|--------------------------------| |-----------------------|  $'
 str_1_row_14 db '|   MATEMATICAS   |       |       |       |        | |    ASISTENCIAS   |    |  $'
 str_1_row_15 db '|-----------------|--------------------------------| |-----------------------|  $'
-str_1_row_16 db '|     QUIMICA     |       |       |       |        | | % DE ASISTENCIA  | 100|  $'
+str_1_row_16 db '|     QUIMICA     |       |       |       |        | | % DE ASISTENCIA  |    |  $'
 str_1_row_17 db '|-----------------|--------------------------------| |-----------------------|  $'
 str_1_row_18 db '|     HISTORIA    |       |       |       |        |                            $'
 str_1_row_19 db '|--------------------------------------------------| |-----------------------|  $'
@@ -102,10 +102,10 @@ str_1_row_21 db '  |--------------------|   |--------------------|    |---------
 str_1_row_22 db '  |  PROMOVIDO   |     |   | NO PROMOVIDO |     |    |                       |  $'
 str_1_row_23 db '  |--------------------|   |--------------------|    |-----------------------|  $'
 
+str_x db 'X$'
 ; -----------------------------------------
 ; VAR OPC2
 ; -----------------------------------------
-
 
 .code
 mov ax, @data
@@ -120,24 +120,16 @@ mov dx, 10
 int 21h
 endm ; SALTO DE LÍNEA
 
-println macro str
-mov ah, 09
-lea dx, offset str
-int 21h
-br
-endm ; MACRO PARA IMPRIMIR LÍNEA
-
 print macro str
 mov ah, 09
 lea dx, offset str
 int 21h
 endm ; MACRO PARA IMPRIMIR SIN SALTO
 
-printat macro str, x, y
-mov ah, 09
-lea dx, x y offset str
-int 21h
-endm
+println macro str
+print str
+br
+endm ; MACRO PARA IMPRIMIR LÍNEA
 
 printintvar macro var
 mov al,var
@@ -154,9 +146,21 @@ add dl,30h
 int 21h
 endm ; MACRO PARA IMPRIMIR INT VAR
 
-printstrvar macro var
+movecursor macro ncol, nrow
+mov dl, ncol
+mov dh, nrow
+mov bh, 0
+mov ah, 02h
+int 10h
+endm ; MACRO PARA MOVER CURSOR
 
-endm ; MACRO PARA IMPRIMIR STR VAR
+recovercursor macro
+mov dl, 1
+mov dh, 24
+mov bh, 0
+mov ah, 02h
+int 10h
+endm ; MACRO PARA RECUPERAR CURSOR
 
 clear macro
 mov ah, 0
@@ -185,6 +189,27 @@ mov cl, n2
 cmp bl, cl
 endm ; MACRO PARA COMPARAR
 
+sum macro n1, n2, res
+mov al, n1
+add al, n2
+mov res, al
+endm ; MACRO PARA SUMAR
+
+mult macro n1, n2, res
+mov al, n1
+mov bl, n2
+mul bl
+mov res, al
+endm ; MACRO PARA MULTIPLICAR
+
+divi macro n1, n2, res
+xor ax, ax
+mov al, n1
+mov bl, n2
+div bl
+mov res, al
+endm ; MACRO PARA DIVIDIR
+
 ; -----------------------------------------
 ; FIN ÁREA MACROS
 ; -----------------------------------------
@@ -203,14 +228,9 @@ println str_menu_5
 println str_menu_6
 println str_menu_7
 print str_menu_8
-
 input_int n_opc
-br
-
-br
 
 ; SALTOS
-
 comp n_opc, 1
 je opc_1
 jl menu
@@ -227,28 +247,178 @@ je opc_6
 comp n_opc, 7
 je salir
 jg menu
-; SALTOS
-
 ; -----------------------------------------
 ; FIN MENÚ
 ; -----------------------------------------
 
+; -----------------------------------------
+; OPCIÓN 1
+; -----------------------------------------
 opc_1:
 clear
 ;PREGUNTAS
-println str_1_title
-print str_1_nombres
-input_str var_nombres
-print str_1_appat
-input_str var_appat
-print str_1_apmat
-input_str var_apmat
+; println str_1_title
+; print str_1_nombres
+; input_str var_nombres
+; print str_1_appat
+; input_str var_appat
+; print str_1_apmat
+; input_str var_apmat
+; print str_1_nomesc
+; input_str var_nomesc
+; print str_1_apmat
+; input_str var_apmat
+; print str_1_apmat
+; input_str var_apmat
+; print str_1_apmat
+; input_str var_apmat
+; print str_1_nomesc
+; input_str var_nomesc
+; print str_1_grupo
+; input_str var_grupo
+; print str_1_turno
+; input_str var_turno
+; print str_1_nctrl
+; input_str var_nctrl
+print str_1_mate1
+input_int var_mate1
+br
+print str_1_mate2
+input_int var_mate2
+br
+print str_1_mate3
+input_int var_mate3
+br
+print str_1_quim1
+input_int var_quim1
+br
+print str_1_quim2
+input_int var_quim2
+br
+print str_1_quim3
+input_int var_quim3
+br
+print str_1_hist1
+input_int var_hist1
+br
+print str_1_hist2
+input_int var_hist2
+br
+print str_1_hist3
+input_int var_hist3
+br
+print str_1_asist
+input_int var_asist
+br
 
-; IMPRESIÓN BOLETA
+; CÁLCULO DE PROMEDIOS
+sum var_mate1, var_mate2, var_mate_p
+sum var_mate_p, var_mate3, var_mate_p
+divi var_mate_p, 3, var_mate_p
 
+sum var_quim1, var_quim2, var_quim_p
+sum var_quim_p, var_quim3, var_quim_p
+divi var_quim_p, 3, var_quim_p
 
+sum var_hist1, var_hist2, var_hist_p
+sum var_hist_p, var_hist3, var_hist_p
+divi var_hist_p, 3, var_hist_p
+
+sum var_mate_p, var_quim_p, var_prom
+sum var_prom, var_hist_p, var_prom
+divi var_prom, 3, var_prom
+
+mult var_asist, 10d, var_porc_asist
+
+; IMPRESIÓN BOLETA (FORMATO)
+clear
+print str_1_row_02
+print str_1_row_03
+print str_1_row_04
+print str_1_row_05
+print str_1_row_06
+print str_1_row_07
+print str_1_row_08
+print str_1_row_09
+print str_1_row_10
+print str_1_row_11
+print str_1_row_12
+print str_1_row_13
+print str_1_row_14
+print str_1_row_15
+print str_1_row_16
+print str_1_row_17
+print str_1_row_18
+print str_1_row_19
+print str_1_row_20
+print str_1_row_21
+print str_1_row_22
+print str_1_row_23
+
+; IMPRESIÓN BOLETA (VARIABLES)
+movecursor 11, 1
+print var_appat
+movecursor 36, 1
+print var_apmat
+movecursor 60, 1
+print var_nombres
+movecursor 6, 4
+print var_nomesc
+movecursor 36, 4
+print var_grupo
+movecursor 50, 4
+print var_turno
+movecursor 68, 4
+print var_nctrl
+movecursor 21, 12
+printintvar var_mate1
+movecursor 29, 12
+printintvar var_mate2
+movecursor 37, 12
+printintvar var_mate3
+movecursor 46, 12
+printintvar var_mate_p
+movecursor 21, 14
+printintvar var_quim1
+movecursor 29, 14
+printintvar var_quim2
+movecursor 37, 14
+printintvar var_quim3
+movecursor 46, 14
+printintvar var_quim_p
+movecursor 21, 16
+printintvar var_hist1
+movecursor 29, 16
+printintvar var_hist2
+movecursor 37, 16
+printintvar var_hist3
+movecursor 46, 16
+printintvar var_hist_p
+movecursor 64, 20
+printintvar var_prom
+movecursor 74, 12
+printintvar var_asist
+movecursor 74, 14
+printintvar var_porc_asist
+
+comp var_porc_asist, 80
+jl noprom
+comp var_prom, 7
+jl noprom
+
+prom:
+movecursor 20, 20
+print str_x
 jmp minimenu
 
+noprom:
+movecursor 44, 20
+print str_x
+jmp minimenu
+
+; -----------------------------------------
+; OPCIÓN 2
+; -----------------------------------------
 opc_2:
 clear
 println str_test
@@ -256,6 +426,9 @@ printintvar n_opc
 br
 jmp minimenu
 
+; -----------------------------------------
+; OPCIÓN 3
+; -----------------------------------------
 opc_3:
 clear
 println str_test
@@ -263,6 +436,9 @@ printintvar n_opc
 br
 jmp minimenu
 
+; -----------------------------------------
+; OPCIÓN 4
+; -----------------------------------------
 opc_4:
 clear
 println str_test
@@ -270,6 +446,9 @@ printintvar n_opc
 br
 jmp minimenu
 
+; -----------------------------------------
+; OPCIÓN 5
+; -----------------------------------------
 opc_5:
 clear
 println str_test
@@ -277,6 +456,9 @@ printintvar n_opc
 br
 jmp minimenu
 
+; -----------------------------------------
+; OPCIÓN 6
+; -----------------------------------------
 opc_6:
 clear
 println str_test
@@ -284,7 +466,11 @@ printintvar n_opc
 br
 jmp minimenu
 
+; -----------------------------------------
+; MINI MENÚ
+; -----------------------------------------
 minimenu:
+recovercursor
 print str_minimenu
 input_int n_opc
 br
@@ -292,6 +478,9 @@ comp n_opc, 1
 je menu
 jmp salir
 
+; -----------------------------------------
+; SALIDA
+; -----------------------------------------
 salir:
 clear
 .exit
